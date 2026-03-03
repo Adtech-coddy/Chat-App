dbebb82cc7f99cbe1d3f75dd36fe197bc15f224c
Adtech <ademolanurudeen65@gmail.com>
2026-03-03 13:56:51 +0100

    fix users route path

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const Message = require('../models/Message');
+const User = require('../models/user')
+const Message = require('../models/message');
 
 // Auth middleware
 const auth = async (req, res, next) => {
