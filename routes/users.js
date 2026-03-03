9917abad0d13191e1864c2cff87a0715f2e93056
Adtech <ademolanurudeen65@gmail.com>
2026-03-03 14:07:41 +0100

    fix model filename casing

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const User = require('../models/User')
 const Message = require('../models/Message');
 
 // Auth middleware
